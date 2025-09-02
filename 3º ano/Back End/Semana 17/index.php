da4c2abedaca1a511807792e3f4ee16bf6bb4a0c
Pâmela <pamelabarros0116@gmail.com>
2025-09-02 15:29:10 -0300

    Atividades 02/09
    atividades que fiz durante a semana de back end de php

---
+++ 3º ano/Back End/Semana 17/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividades da semana 17</title>
+</head>
+<body>
+    <?php
+echo "<h2>Ideia 1: GET vs POST</h2>";
+echo "<b>Gancho:</b> Você mandaria sua senha num CARTÃO POSTAL?<br>";
+echo "<b>Conceito:</b> Diferença entre GET e POST (visibilidade e segurança).<br>";
+echo "<b>Analogia/Exemplo:</b> Mostrar um cartão postal (GET), onde todo mundo vê a mensagem, versus uma carta fechada em envelope (POST), que guarda a informação segura.<br><br>";
+
+echo "<h2>Ideia 2: Superglobais (\$_GET e \$_POST)</h2>";
+echo "<b>Gancho:</b> Quer saber como o PHP lê sua mensagem?<br>";
+echo "<b>Conceito:</b> Superglobais \$_GET e \$_POST como 'caixinhas' onde chegam os dados.<br>";
+echo "<b>Analogia/Exemplo:</b> Mostrar caixas de correio com etiquetas “GET” e “POST”. Cada uma recebe cartas diferentes dependendo do tipo de envio do formulário.<br><br>";
+
+echo "<h2>Ideia 3: Validação de formulário</h2>";
+echo "<b>Gancho:</b> E se alguém tentar mandar um formulário vazio? <br>";
+echo "<b>Conceito:</b> Validar dados recebidos em PHP antes de usar.<br>";
+echo "<b>Analogia/Exemplo:</b> Mostrar uma catraca de entrada que só libera quem tem ingresso. Se o formulário está vazio → trava! Se tem os dados certos → passa sem problema.<br>";
+?>
+
+</body>
+</html>
---
+++ 3º ano/Back End/Semana 18/atividade02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área do Trapézio</title>
+</head>
+<body>
+    <?php 
+     $baseMaior = 10 ;
+     $baseMenor = 6 ;
+     $altura = 4 ;
+
+     $resultado = (($baseMaior + $baseMenor) * $altura) / 2 ;
+    
+     echo "A área do trapézio é: $resultado "
+    ?>
+</body>
+</html>
